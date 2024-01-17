a9f5c1473bb8d32d1c4f98350be0c6c58c1e0dcc
fengyunjue <ma772528138@qq.com>
2024-01-17 14:24:33 +0800

    fix somethings

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,2 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public struct Logger {
+    var DEBUG: Bool = false
+    
+    // 这里是 struct，所以按照 swift 的 struct 语法，可以不写 init，
+    // 但是，因为是封装成 SPM 库，需要对外暴露，
+    // 所以 init 构造器不能默认不写，一定需要有 public 构造器，否则外部无法初始化
+    public init() {
+    }
+    
+    public init(_ DEBUG: Bool) {
+        self.DEBUG = DEBUG
+    }
+    
+    public func log(_ msg: String) {
+        if DEBUG {
+            print("Logger\t\(msg)")
+        }
+    }
+}
+
